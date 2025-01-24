737061fe172ba494dd25adc96a074f916edae30c
YooNice100 <euc003@ucsd.edu>
2025-01-23 23:40:46 -0800

    changed stuff

--- global.js
+++ global.js
@@ -1,19 +1,56 @@
 console.log('IT’S ALIVE!');
 
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
 
-let navLinks = $$("nav a");
+// let navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
 
 
-if (currentLink){
-    currentLink.classList.add('current');
-}
+// if (currentLink){
+//     currentLink.classList.add('current');
+// }
   
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume'},
+    { url: 'contact/', title: 'Contact'},
+    { url: 'https://github.com/YooNice100', title: 'GitHub'}
+
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // TODO create link and add it to nav
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+    // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+
+    nav.append(a);
+  }
+
+
+
+
 
