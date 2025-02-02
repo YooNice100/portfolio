fd4bcc61791493870514288ed5d9ee48fc337460
Eunice <eunicecho100@gmail.com>
2025-02-01 23:59:17 -0800

    stuff

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
 
     
 
-
-const projects = await fetchJSON('./lib/projects.json');
+async function loadGitHubData(){
+  try{
+    const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 
@@ -24,4 +25,34 @@ if (profileStats) {
             <dt>Following:</dt><dd>${githubData.following}</dd>
           </dl>
       `;
+  } else {
+    console.error("github data could not be loaded");
   }
+
+  }catch(error){
+    console.error('github data fetch error', error);
+  }
+}
+
+loadGitHubData();
+// const projects = await fetchJSON('./lib/projects.json');
+// const latestProjects = projects.slice(0, 3);
+// const projectsContainer = document.querySelector('.projects');
+
+// renderProjects(latestProjects, projectsContainer, 'h2');
+
+// const githubData = await fetchGithubData('YooNice100');
+
+
+// const profileStats = document.querySelector('#profile-stats');
+
+// if (profileStats) {
+//     profileStats.innerHTML = `
+//           <dl>
+//             <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+//             <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+//             <dt>Followers:</dt><dd>${githubData.followers}</dd>
+//             <dt>Following:</dt><dd>${githubData.following}</dd>
+//           </dl>
+//       `;
+//   }
