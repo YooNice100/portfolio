ffc6c8a2d0e462dcfe63b96359fe8a27e74bc946
Eunice <eunicecho100@gmail.com>
2025-02-07 22:31:14 -0800

    stuff

--- global.js
+++ global.js
@@ -24,7 +24,7 @@ for (let p of pages) {
     let url = p.url;
     let title = p.title;
     // TODO create link and add it to nav
-    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    // url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
 
     // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
     let a = document.createElement('a');
